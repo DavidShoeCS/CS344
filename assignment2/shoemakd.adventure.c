7db0381a42e58618de741e0bf3ecc151a7c12935
David Shoemaker <davids@Davids-MacBook-Air.local>
2020-02-06 20:08:15 -0800

    made structs from file garbage

--- assignment2/shoemakd.adventure.c
+++ assignment2/shoemakd.adventure.c
@@ -22,28 +22,48 @@ typedef struct roomNode{  /*Structure of a room.  Holds the values a room has*/
 
 }roomNode;
 
-
+roomNode* initRooms();
 
 char *getNewestDir();
+int findLine(char const* str, char const* substr);
+void printRoomsHelper(roomNode *listOfRooms);
+roomNode* findNode(roomNode *roomList, char *roomName);
 
-void buildStructsFromFile(char *directory);
+void buildStructsFromFile(char *directory, roomNode* roomList);
 
-
+/**********************MAIN AREA*************************************************/
 int main(int argc, char *argv[]){
 
 char *newestDir; /*latest directory we found from function */
 
 newestDir = getNewestDir();
-/*printf("%s\n", newestDir);*/
 
-buildStructsFromFile(newestDir);
+roomNode *roomList = initRooms();
 
+buildStructsFromFile(newestDir, roomList);
+
+printRoomsHelper(roomList);
 
 return 0;
 }
+/**********************END MAIN AREA********************************************/
 
 
-void buildStructsFromFile(char *directory){
+
+int findLine(char const* str, char const* substr){
+
+  char* position = strstr(str, substr);
+    if(position) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+
+void buildStructsFromFile(char* directory, roomNode* roomList){
+
   struct dirent *de;  // Pointer for directory entry
 
     char *dirPath[100];
@@ -60,12 +80,11 @@ void buildStructsFromFile(char *directory){
         printf("Could not open current directory" );
     }
 
-
-
     FILE *fp;
     char line[1000];
     char strStorage[100];
-
+    int i=0;
+    int conCounter=0;
     while ((de = readdir(dir)) != NULL) /*read every file in our directory*/
           if(de->d_name[0] != '.'){
             strcpy(filePath, "\0");
@@ -73,27 +92,66 @@ void buildStructsFromFile(char *directory){
             strcat(filePath, directory);
             strcat(filePath, "/");
             strcat(filePath, de->d_name); /*keep catting on filePath to get the right file directory*/
-            printf("-file path: %s\n", filePath);
-
 
             fp = fopen(filePath, "r");
+            conCounter=0;
             while (fgets(line, 1000, fp) != NULL) {
+
                 sscanf(line, "%*[^:]: %[^]%*[^\n]", strStorage); /* from google.  takes a line from our file, regex it to remove everthing before ":", then store the new line into strStorage.*/
-                printf("%s\n", strStorage);
-                /*NEXT STEP: print to a struct*/
+
+                if(findLine(line, "ROOM NAME") == 1){
+                  strcpy(roomList[i].rName, strStorage);
+                }
+                if(findLine(line, "ROOM TYPE") == 1){
+                  roomList[i].rType = strStorage[0];
+                }
+                if(findLine(line, "CONNECTION") == 1){ /*set the amount of connections of each room*/
+                  conCounter++;
+                  roomList[i].numCons = conCounter;
+                }
             }
-
-            printf("\n");
+            i++;
             fclose(fp);
-
-
-
-            /*printf("%s\n", de->d_name);*/
-
-
           }
+
     closedir(dir);
 
+
+    /*************************************************************************************/
+    dir = opendir(dirPath); /*This section will re open all the files, and fill in connections*/
+    if (dir == NULL)  // if the directory doesn't exist, throw error
+    {
+        printf("Could not open current directory" );
+    }
+
+
+    i=0;
+    conCounter=0;
+    while ((de = readdir(dir)) != NULL) /*read every file in our directory*/
+          if(de->d_name[0] != '.'){
+            strcpy(filePath, "\0");
+            strcat(filePath, "./");
+            strcat(filePath, directory);
+            strcat(filePath, "/");
+            strcat(filePath, de->d_name); /*keep catting on filePath to get the right file directory*/
+
+            fp = fopen(filePath, "r");
+            conCounter=0;
+            while (fgets(line, 1000, fp) != NULL) {
+
+                sscanf(line, "%*[^:]: %[^]%*[^\n]", strStorage); /* from google.  takes a line from our file, regex it to remove everthing before ":", then store the new line into strStorage.*/
+                if(findLine(line, "CONNECTION") == 1){
+
+                  roomList[i].cons[conCounter] = findNode(roomList, strStorage);
+
+                  conCounter++; /*Keep track of what room we are looking at*/
+          }
+
+        }
+        i++;
+        fclose(fp);
+        }
+      closedir(dir);
 }
 
 
@@ -116,6 +174,43 @@ char *getNewestDir(){
       return latestDir;
 
   closedir(dir);
+}
 
+roomNode* initRooms(){
+  roomNode* listOfRooms = malloc(NUM_OF_ROOMS * sizeof(roomNode));  /*create a list of room nodes*/
+  int i;
+  for(i=0; i< NUM_OF_ROOMS; i++){
+    listOfRooms[i].numCons=0;
+  }
+
+  return listOfRooms;
+
+}
+
+void printRoomsHelper(roomNode *listOfRooms){
+
+  int j;
+  int i;
+  for(i=0; i < NUM_OF_ROOMS; i++){
+
+  printf("\nRoom: %s's number of Cons: %d", listOfRooms[i].rName, listOfRooms[i].numCons);
+  printf("\nRoom: %s(%c) connects to: ", listOfRooms[i].rName, listOfRooms[i].rType);
+  for(j=0; j<listOfRooms[i].numCons; j++){
+    printf("%s, ", listOfRooms[i].cons[j]->rName);
+  }
+  printf("\n");
+
+  }
+
+}
+
+roomNode* findNode(roomNode *roomList, char *roomName){
+  int m;
+  for(m=0; m<NUM_OF_ROOMS; m++){
+    if(strcmp(roomList[m].rName, roomName)==0){
+      return &roomList[m];
+  }
+  }
+  return NULL;
 
 }
